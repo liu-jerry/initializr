3fbdc6dfc5ef02225025a84c56fcbc5e57697aa0
liuzhenyuan <liuzhenyuan@meituan.com>
2015-07-23 13:44:15 +0800

    add

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 编译部署脚本
+
+# 获取当前目录
+BASE_DIR=$(dirname $0)
+
+# 编译
+cd $BASE_DIR/initializr && mvn install 
+cd $BASE_DIR/initializr-service && spring jar start.jar app.groovy
+
+# 停掉老版本
+docker -H 172.27.2.136:3376 rm -f start
+
+# 构建image
+docker -H 172.27.2.136:3376 build -t initializr initializr
+
+# 运行
+docker -H 172.27.2.136:3376 run -d --name start -p 8088:8080 initializr
